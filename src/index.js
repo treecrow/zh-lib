3d5a9d070d6aa85889e4d04922e60e561f3c3dcf
treecrow <drytreecrow@gmail.com>
2017-11-20 14:16:44 +0800

    babel add

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import array from './array'
+import date from './date'
+import dom from './dom'
+import string from './string'
+import sundry from './sundry'
+
+export default {
+  array,
+  date,
+  dom,
+  string,
+  sundry,
+}
